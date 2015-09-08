0f9f79a02e29b3e00663a4e0e3d29bf060efa45f
leftfire <leftfire@hotmail.com>
2015-09-08 13:51:11 +0800

    21

---
+++ src/main/java/com/spring/entity/UserEntity.java
@@ -0,0 +1,47 @@
+package com.spring.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//用户实体
+@Table(name="user")
+@Entity
+public class UserEntity {
+	private int id;
+	private String username;
+	private String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID",nullable=false,length=16)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="username",length=16,nullable=false)
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	@Column(name="password",length=32,nullable=false)
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	@Override
+	public String toString(){
+		return "";
+	}
+}
